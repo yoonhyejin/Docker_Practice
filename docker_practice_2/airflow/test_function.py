50cfb358c321af4694013c103e3f3b8a608fdaad
dini <0327jane@gmail.com>
2021-08-26 11:36:45 +0900

    host 수정 및 json 오류 수정 완료

--- docker_practice_2/airflow/test_function.py
+++ docker_practice_2/airflow/test_function.py
@@ -5,6 +5,8 @@ from pendulum import timezone
 url = 'http://localhost:5000/insert'
 
 def insert_data(text):
-    requests.post(url=url, data={ 'text' : text })
-    
+    datas = { 'text' : text }
+    x = requests.post(url=url, json=datas)
+    print(x.text) 
+
 insert_data(text='First data using test_func')
